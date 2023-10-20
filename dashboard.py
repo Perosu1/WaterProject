6fcfbdc661a79cfde5da4bbc8e0224ca081d8f4c
b-gleb <lion.zz@mail.ru>
2023-10-20 20:25:43 +0100

    Add tabs for vars, MSD, RDF

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # Import data
-data = pd.read_csv(
+data_log = pd.read_csv(
     'data.log',
     sep = '\s+',
     engine = 'python',
@@ -12,6 +12,26 @@ data = pd.read_csv(
     header = 0
 )
 
+
+data_msd = pd.read_csv(
+    'MSD.out',
+    sep = '\s+',
+    comment = '#',
+    header = None
+)
+msd_column_names = ["TimeStep", "<x^2>", "<y^2>", "<z^2>", "<R^2>"]
+data_msd.columns = msd_column_names
+
+
+data_rdf = pd.read_csv(
+    'RDF.out',
+    sep = '\s+',
+    skiprows = 4,
+    header = None
+)
+rdf_column_names = ["Row", "Distance", "HH RDF", "HH CN", "HO RDF", "HO CN", "OH RDF", "OH CN", "OO RDF", "OO CN"]
+data_rdf.columns = rdf_column_names
+
 # Initialize the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets = external_stylesheets)
@@ -19,22 +39,22 @@ app = Dash(__name__, external_stylesheets = external_stylesheets)
 
 # FIGURES
 def fig_parameter(parameter: str, colour: str):
-    average = data[parameter].mean()
+    average = data_log[parameter].mean()
 
     # Create line plot
-    fig = px.line(data, x = 'Time', y = parameter, color_discrete_sequence = [colour])
+    fig = px.line(data_log, x = 'Time', y = parameter, color_discrete_sequence = [colour])
 
     # Add average line
     fig.add_hline(y = average, line_dash = "dot", annotation_text = f'Average {parameter}: {average:.2f}', annotation_position="top right")
     
     # Average error region
-    fig.add_hrect(y0 = average - data.sem()[parameter], y1 = average + data.sem()[parameter], line_width=0, fillcolor="red", opacity=0.2)
+    fig.add_hrect(y0 = average - data_log.sem()[parameter], y1 = average + data_log.sem()[parameter], line_width=0, fillcolor="red", opacity=0.2)
 
     return fig
 
 
 # BASIC STATS
-stats_df = data.iloc[:, 2:].agg(['mean', 'sem'])
+stats_df = data_log.iloc[:, 2:].agg(['mean', 'sem'])
 # Transpose the result for better visualization
 stats_df = stats_df.transpose().reset_index()
 stats_df.columns = ['Variable', 'Mean', 'Error']
@@ -46,63 +66,79 @@ stats_df['Lower'] = (stats_df['Mean'] - stats_df['Error']).round(5)
 
 # App layout
 app.layout = html.Div([
-    html.Div(className='row', children='LAMMPS Dashboard',
-             style={'textAlign': 'center', 'color': '#07215d', 'fontSize': 30}),
+    html.H1('LAMMPS Dashboard', style={'textAlign': 'center', 'color': '#07215d', 'fontSize': 35}),
 
-    # Row 1
-    html.Div(className='row', children=[
-        html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('Temp', 'rgb(99, 113, 241)'))],
-        style = {'width': '30%'}
-        ),
 
-        html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('Density', 'rgb(222, 96, 70)'))],
-        style = {'width': '30%'}
-        ),
+    dcc.Tabs([
+        dcc.Tab(label='Variables', children=[
+                # Row 1
+                html.Div(className='row', children=[
+                    html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('Temp', 'rgb(99, 113, 241)'))],
+                    style = {'width': '30%'}
+                    ),
 
-        html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('KinEng', 'rgb(91, 200, 154)'))],
-        style = {'width': '30%'}
-        ),
-    ]),
+                    html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('Density', 'rgb(222, 96, 70)'))],
+                    style = {'width': '30%'}
+                    ),
 
-    # Row 2
-    html.Div(className='row', children=[
-        html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('PotEng', 'rgb(160, 106, 242)'))],
-        style = {'width': '30%'}
-        ),
+                    html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('KinEng', 'rgb(91, 200, 154)'))],
+                    style = {'width': '30%'}
+                    ),
+                ]),
 
-        html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('TotEng', 'rgb(243, 164, 103)'))],
-        style = {'width': '30%'}
-        ),
+                # Row 2
+                html.Div(className='row', children=[
+                    html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('PotEng', 'rgb(160, 106, 242)'))],
+                    style = {'width': '30%'}
+                    ),
 
-        html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('Volume', 'rgb(97, 209, 239)'))],
-        style = {'width': '30%'}
-        ),
-    ]),
+                    html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('TotEng', 'rgb(243, 164, 103)'))],
+                    style = {'width': '30%'}
+                    ),
 
-    # Row 3
-    html.Div(className='row', children=[
-        html.Div(className='row', children=[
-            dcc.RangeSlider(data['Time'].min(), data['Time'].max()/2, 100, value=[data['Time'].min(), data['Time'].max()], id='range-slider')
-        ],
-        style = {'width': '90%', 'margin': 'auto'}),
+                    html.Div(className='three columns', children=[dcc.Graph(figure=fig_parameter('Volume', 'rgb(97, 209, 239)'))],
+                    style = {'width': '30%'}
+                    ),
+                ]),
 
-        html.Div(className='two columns', children=[
-            dcc.Checklist(options = ['Temp', 'Density', 'KinEng', 'PotEng', 'TotEng', 'Volume'], value = ['Temp', 'Density', 'KinEng', 'PotEng', 'TotEng'], inline = True, id='line-variables'),
-            dcc.Checklist(options = ['Averages'], inline = True, id='line-averages'),
+                # Row 3
+                html.Div(className='row', children=[
+                    html.Div(className='row', children=[
+                        dcc.RangeSlider(data_log['Time'].min(), data_log['Time'].max()/2, 100, value=[data_log['Time'].min(), data_log['Time'].max()], id='range-slider')
+                    ],
+                    style = {'width': '90%', 'margin': 'auto'}),
 
-            dcc.Graph(figure={}, id='all-variables')
-        ],
-        style = {'width': '45%', 'margin': 'auto'}),
+                    html.Div(className='two columns', children=[
+                        dcc.Checklist(options = ['Temp', 'Density', 'KinEng', 'PotEng', 'TotEng', 'Volume'], value = ['Temp', 'Density', 'KinEng', 'PotEng', 'TotEng'], inline = True, id='line-variables'),
+                        dcc.Checklist(options = ['Averages'], inline = True, id='line-averages'),
 
-        html.Div(className='two columns', children = [
-            dash_table.DataTable(data=stats_df.to_dict('records'), id = 'stats_df')
-        ],
-        style = {'width': '45%'})
-    
+                        dcc.Graph(figure={}, id='all-variables')
+                    ],
+                    style = {'width': '45%', 'margin': 'auto'}),
+
+                    html.Div(className='two columns', children = [
+                        dash_table.DataTable(data=stats_df.to_dict('records'), id = 'stats_df')
+                    ],
+                    style = {'width': '45%'})
+                ])
+        ]),
+
+        dcc.Tab(label='MSD', children=[
+            html.Div(className='row', children=[
+                dcc.Graph(figure = px.line(data_msd, x = 'TimeStep', y =  msd_column_names[1:]))
+            ],
+            style = {'width': '50%'}),
+        ]),
+
+        dcc.Tab(label='RDF', children=[
+            html.Div(className='row', children=[
+                dcc.Graph(figure = px.line(data_rdf, x = 'Distance', y =  rdf_column_names[2:]))
+            ],
+            style = {'width': '50%'}),
+        ]),
+
     ])
-
 ])
-
 
 
 # CONTROLS
@@ -116,7 +152,7 @@ app.layout = html.Div([
 def update_graph(col_chosen, range, average):
 
     # Change data range
-    range_data = data.iloc[int(range[0]/10):,:]
+    range_data = data_log.iloc[int(range[0]/10):,:]
 
     # Create main plot
     fig = px.line(range_data, x = 'Time', y = col_chosen)
